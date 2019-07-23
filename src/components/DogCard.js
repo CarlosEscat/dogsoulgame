2ccb70a7343c8188dc5922becb9958e27917e613
CarlosEscat <carlosescat@gmail.com>
2019-07-23 16:22:33 +0200

    CSS conflict fixed again

--- src/components/DogCard.js
+++ src/components/DogCard.js
@@ -21,8 +21,8 @@ class DogCard extends Component {
   render(){
     const { breed } = this.props
     return (
-      <div>
-        { this.state.url ? <img src={this.state.url} alt="Dog" key={`${breed}1`}/> : "Woof Woof"}
+      <div >
+        { this.state.url ? <img className='listImages' src={this.state.url} alt="Dog" key={`${breed}1`}/> : "Woof Woof"}
         <li key={breed}>{<Link to={`/dog-breeds/${breed}`}>{breed}</Link>}</li>
       </div>
     )
