c74e6e2c050871cb8249d231ae5faff13116e93d
nataliaev <nataliiaevchenko@gmail.com>
2019-07-25 16:59:20 +0200

    Added redux thunks

--- src/reducers/breeds.js
+++ src/reducers/breeds.js
@@ -1,8 +1,6 @@
-import { SET_BREED_STATE } from '../actions';
-
 export default (state = [], { type, payload }) => {
   switch (type) {
-    case SET_BREED_STATE:
+    case 'SET_BREED_STATE':
       return payload;
     default:
       return state;
