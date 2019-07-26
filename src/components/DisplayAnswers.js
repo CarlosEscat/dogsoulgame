86025a8c282f998964e3e9b4ea8b6afce6a2c72d
CarlosEscat <carlosescat@gmail.com>
2019-07-26 10:12:57 +0200

    eliminating id identification hint button

--- src/components/DisplayAnswers.js
+++ src/components/DisplayAnswers.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import { addUserAnswer } from '../actions/userAnswers';
-import AnswerButton from './AnswerButton';
-import { breedsAlreadySeen } from '../actions/BreedOrder';
+import { addUserAnswer } from "../actions/userAnswers";
+import AnswerButton from "./AnswerButton";
+import { breedsAlreadySeen } from "../actions/BreedOrder";
 
-import './DisplayAnswers.css';
+import "./DisplayAnswers.css";
 
 class DisplayAnswers extends React.Component {
   handleClick = event => {
     event.preventDefault();
-    const button1 = document.getElementById('button1');
-    const button2 = document.getElementById('button2');
-    button1.style.visibility = 'visible';
-    button2.style.visibility = 'visible';
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+    button1.style.visibility = "visible";
+    button2.style.visibility = "visible";
 
     if (this.props.answer != null) {
       this.props.breedsAlreadySeen(this.props.answer);
@@ -45,43 +45,53 @@ class DisplayAnswers extends React.Component {
 
   showButtonHint = () => {
     console.log(this.props.answer);
-    document.getElementById('hintButton').style.visibility = 'vissible';
+    document.getElementById("hintButton").style.visibility = "vissible";
     if (
       this.props.breedsLearned.length !== 0 &&
       this.props.breedsLearned.includes(this.props.answer) === false
     ) {
-      document.getElementById('hintButton').style.visibility = 'vissible';
+      document.getElementById("hintButton").style.visibility = "vissible";
     } else if (this.props.breedsLearned.includes(this.props.answer) === true) {
       console.log(
-        'Entered if',
-        this.props.breedsLearned + ' ' + this.props.answer
+        "Entered if",
+        this.props.breedsLearned + " " + this.props.answer
       );
-      document.getElementById('hintButton').style.visibility = 'hidden';
+      document.getElementById("hintButton").style.visibility = "hidden";
     }
   };
 
   showHint = () => {
     const correctAnswer = this.props.answer;
-    const button1 = document.getElementById('1');
-    const button2 = document.getElementById('2');
+    const button1 = document.getElementById("1");
+    const button2 = document.getElementById("2");
 
     if (button1.value !== correctAnswer) {
-      button1.style.visibility = 'hidden';
+      button1.style.visibility = "hidden";
     } else if (button2.value !== correctAnswer) {
-      button2.style.visibility = 'hidden';
+      button2.style.visibility = "hidden";
     }
   };
 
   answersArray = () => {
     if (Array.isArray(this.props.gameOptions))
       return this.props.gameOptions.sort(() => Math.random() - 0.5);
-    else return ['But wait there is more!!!'];
+    else return ["But wait there is more!!!"];
   };
 
   render() {
+    const { answer, breedsLearned } = this.props;
+    const isVisible = !breedsLearned.includes(answer);
     return (
       <div>
-        <button id="hintButton" className="hint-button" onClick={this.showHint}>
+        {/* <button id="hintButton" className="hint-button" onClick={this.showHint}>
+          Hint
+        </button> */}
+
+        <button
+          id="hintButton"
+          className={`hint-button ${isVisible ? "visible" : "hidden"}`}
+          onClick={this.showHint}
+        >
           Hint
         </button>
 
